253b1642b2609e6ca666d53a69aa9f02bc99db9f
loonaire <36820703+loonaire@users.noreply.github.com>
2023-09-09 13:37:12 +0200

    Update client.go
    Ajout de la reception des paquet via un reader

--- tcpserver/client.go
+++ tcpserver/client.go
@@ -21,6 +21,7 @@ type client struct {
 	name            string
 }
 
+//Reception à l'aide d'un scanner:
 func (c *client) recv() {
 	input := bufio.NewScanner(c.conn)
 	for input.Scan() {
@@ -38,6 +39,34 @@ func (c *client) recv() {
 	}
 	close(c.inch)
 }
+
+/*
+// Reception d'un paquet en utilisant un Reader:
+func (c *client) recv() {
+	// test reader:
+	reader := bufio.NewReader(c.conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				var netErr net.Error
+				if errors.As(err, &netErr) && netErr.Timeout() {
+					log.Println(err)
+				}
+				return
+			}
+		}
+		if len(msg) > 0 {
+			msg := strings.TrimRight(msg, "\n")
+			slog.Debug(fmt.Sprintf("%s - << %q", c.conn.RemoteAddr(), msg))
+			c.inch <- string(msg)
+		}
+	}
+	close(c.inch)
+}
+*/
 
 func (c *client) writer() {
 	for msg := range c.outch {
